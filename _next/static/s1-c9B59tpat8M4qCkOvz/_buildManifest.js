c1dc1ea3567d93572f94db9d9067d1bf685cb7dc
imoken777 <imoken777@users.noreply.github.com>
2023-05-28 13:08:54 +0000

    deploy: febcf6967d8bd38434e13e02ed81edad118b30f4

---
+++ _next/static/s1-c9B59tpat8M4qCkOvz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/63e61697b847fccf.css","static/chunks/pages/index-d47ef73d124af5ba.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
