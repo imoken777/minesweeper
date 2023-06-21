6ff3d12ef142ffecb10e717106bc446e13b1c033
imoken777 <imoken777@users.noreply.github.com>
2023-06-21 05:28:35 +0000

    deploy: 27753fcdf48d1b9497eb641f16bb4d63a6e7396c

---
+++ _next/static/y8moQ5Helg-mwALZbmtzO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c113650a4aa1d793.css","static/chunks/pages/index-a147e4806018b5df.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
